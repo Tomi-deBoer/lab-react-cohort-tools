bc923e7be099f451ee0b6125c7109f18194b4fd6
Oyetomi Oyelaja <torreyjh@gmail.com>
2026-08-13 22:02:14 +0200

    Add routing

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,4 +1,5 @@
 import placeholderImage from "../assets/profile-icon.png";
+import { Link } from "react-router-dom";
 
 function StudentCard({
   _id,
@@ -11,6 +12,7 @@ function StudentCard({
   className,
 }) {
   return (
+    <Link to={`/students/${_id}`}>
       <div
         className={`StudentCard flex justify-between items-center p-3 mb-2 bg-white shadow-sm rounded border border-gray-200 hover:bg-gray-50 ${className}`}
       >
@@ -28,13 +30,18 @@ function StudentCard({
             }}
           />
         </span>
+
         <span style={{ flexBasis: "20%" }}>
           {firstName} {lastName}
         </span>
+
         <span style={{ flexBasis: "20%" }}>{program}</span>
+
         <span style={{ flexBasis: "20%" }}>{email}</span>
+
         <span style={{ flexBasis: "20%" }}>{phone}</span>
       </div>
+    </Link>
   );
 }
 
--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,5 +1,6 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
+import { Link } from "react-router-dom";
 
 function StudentDetailsPage() {
 
@@ -7,11 +8,14 @@ function StudentDetailsPage() {
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
   const studentId = "1";
-  const studentProfile = studentsData.find((student) => student._id === studentId);
+  const studentProfile = studentsData.find(
+    (student) => student._id === studentId
+  );
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
       <h1>Student Details Page</h1>
+
       <div className="bg-white p-8 rounded-lg shadow-md mb-6">
         {studentProfile && (
           <>
@@ -20,6 +24,7 @@ function StudentDetailsPage() {
               alt="profile-photo"
               className="rounded-full w-32 h-32 object-cover border-2 border-gray-300"
             />
+
             <h1 className="text-2xl mt-4 font-bold absolute">
               {studentProfile.firstName} {studentProfile.lastName}
             </h1>
@@ -65,12 +70,13 @@ function StudentDetailsPage() {
               </p>
             </div>
 
-
             {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            <Link
+              to="/"
+              className="inline-block text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out"
+            >
               Back
-            </button>
-
+            </Link>
           </>
         )}
       </div>
